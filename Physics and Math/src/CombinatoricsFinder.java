6ddb6501b1fc69d9ab892eb4424b5bed05f13a4a
Will Wei <itswillswei@gmail.com>
2018-05-27 22:43:27 -0700

    started combinatoricsfinder

---
+++ Physics and Math/src/CombinatoricsFinder.java
@@ -0,0 +1,16 @@
+
+public class CombinatoricsFinder {
+
+	public static void main(String[] args) {
+		//Initialize values
+		int a = 2;
+		int b = 2;
+		int n = a+b;
+		//There are a number of x and b number of y
+		int x = 3;
+		int y = 8;
+
+		
+	}
+
+}
